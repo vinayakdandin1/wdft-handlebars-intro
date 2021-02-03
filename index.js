4361be8282a3009fe66cec2889aff4c84414fcfb
Vinayak Dandin <vinayakdandin1@gmail.com>
2021-02-03 12:40:54 +0100

    Before activity/lab

--- index.js
+++ index.js
@@ -1,29 +1,68 @@
+require('dotenv').config()
+
 // setup for express
 const express = require("express");
 const app = express();
 
+const port = process.env.PORT;
 
-const port = 3000;
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views')
 
 // require some data form your data.js file
-let {students, instructors} = require('./data')
+let {students, instructors, getStudents, getTeachers} = require('./data')
 
 // just a simple middleware to show you how it works
 // you will always see that console.log when you visit any page
-app.use((req, res, next) => {
-    console.log("Hello im the middleware");
-    next();
-});
+// app.use((req, res, next) => {
+//     console.log("Hello im the middleware");
+//     next();
+// });
 
 // letting your middleware know where to find all static files
 app.use(express.static(__dirname + "/public"));
 
+
 // ROUTES DEFINED BELOW
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + '/views/landing.html')
+
+    let myName = 'Vinayak'
+
+    res.render('landing.hbs', {
+        name: myName, 
+    })
 });
 
+app.get("/students", (req, res) => {
+
+    getStudents()
+        .then((students) => {
+            res.render('students.hbs', {students})
+        })
+        .catch(() => {
+
+        })
+
+
+    //res.render('students.hbs', {students })
+});
+
+app.get("/instructors", (req, res) => {
+    //Fetching all the instructors when the user visits this URL
+    getTeachers()
+        .then((instructors) => {
+            //If successful show the page
+            res.render('instructors.hbs', {instructors})
+        })
+        .catch(() => {
+
+        })
+
+
+
+    //res.render('instructors.hbs', {instructors})
+});
 
 // Express setup to listen for all client requests on a certain port
 app.listen(port, () =>
